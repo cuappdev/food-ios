43b48177e18c4f3b729203a363b7db944e3e51a9
Phillip O'Reggio <pnoreggio@gmail.com>
2021-04-08 23:38:07 -0400

    Start setting up Google Sign In in ContentView

--- Food/ContentView.swift
+++ Food/ContentView.swift
@@ -6,8 +6,52 @@
 //
 
 import SwiftUI
+import GoogleSignIn
+
+private class GoogleSignInManager: NSObject, GIDSignInDelegate {
+
+    var name: String?
+    var email: String?
+    var userId: String?
+
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print("\(error.localizedDescription)")
+            return
+        }
+
+        setUserInformation(user: user)
+    }
+
+    func setUserInformation(user: GIDGoogleUser) {
+        name = user.profile.name
+        email = user.profile.email
+        userId = user.userID
+    }
+
+    // override init() {
+    //     GIDSignIn.sharedInstance().delegate = self
+
+    //     let presentingViewController = UIApplication.shared.windows.last?.rootViewController
+    //     print("presentingViewController: \(presentingViewController)")
+    //     GIDSignIn.sharedInstance()?.presentingViewController = presentingViewController
+    // }
+
+}
 
 struct ContentView: View {
+
+    @State private var signInManager = GoogleSignInManager()
+
+    init() {
+        GIDSignIn.sharedInstance().delegate = signInManager
+
+        let presentingViewController = UIApplication.shared.windows.last?.rootViewController
+        print("presentingViewController: \(presentingViewController)")
+        // TODO this is nil; where to present...
+        GIDSignIn.sharedInstance()?.presentingViewController = presentingViewController
+    }
+
     var body: some View {
         Text("Hello, world!")
             .padding()
--- Food/FoodApp.swift
+++ Food/FoodApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Initialization sign-in
+        GIDSignIn.sharedInstance().clientID = "TODO"
+        return true
+    }
+
+}
 
 @main
 struct FoodApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+
 }
